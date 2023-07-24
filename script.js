8be5d72a19b199aa7e7137b5953baafbf84f760a
a-lil-birb <48155605+a-lil-birb@users.noreply.github.com>
2023-07-24 18:11:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 class NovasTerminal {
     constructor() {
+        this.level = 7
         this.facilities = 1
         this.facilitycap = 100
 
@@ -15,22 +16,32 @@ class NovasTerminal {
 
         this.num_powercells = 0
         this.num_grids = 0
+
+        this.level_to_cap = {
+            1: 5,
+            2: 7,
+            3: 14,
+            4: 28, // unsure
+            5: 50,
+            6: 75,
+            7: 100,
+        };
     }
 
     // [0]Gold, [1]Pentagons, [2]Octagons, [3]Ch1Stacks, [4]WhetStacks
     _compute_output_daily() {
-        return [24000*this.num_goldgens,8*this.num_drills,8*this.num_drills/(5*5*5),24*this.num_t1,8*this.num_twhet]
+        return [24000*this.num_goldgens,8*this.num_drills,8*this.num_drills/(5*5*5),24*this.num_t1,24*this.num_twhet]
     }
     _compute_output_max_capacity() {
         return [60000*this.num_goldgens,10*this.num_drills,10*this.num_drills/(5*5*5),10*this.num_t1,10*this.num_twhet]
     }
     get_daily_output() {
         const [gold, pentagons, octagons, ch1mats, whetstones] = this._compute_output_daily();
-        return `${gold.toLocaleString("en-US")} Gold<br/>${pentagons} Pentagons ⇒ ${octagons} Octagons<br/>${ch1mats} stacks of Ch.1 Mats<br/>${whetstones} stacks of Whetstones`
+        return `${gold.toLocaleString("en-US")} Gold<br/>${pentagons} <img src="images/icons/Pentagon_Gem.png" width="20" height="20"> ⇒ ${octagons} <img src="images/icons/Octagon_Gem.png" width="20" height="20"><br/>${ch1mats} stacks of Ch.1 Mats<br/>${whetstones} stacks of Whetstones`
     }
     get_max_capacity_output() {
         const [gold, pentagons, octagons, ch1mats, whetstones] = this._compute_output_max_capacity();
-        return `${gold.toLocaleString("en-US")} Gold<br/>${pentagons} Pentagons ⇒ ${octagons} Octagons<br/>${ch1mats} stacks of Ch.1 Mats<br/>${whetstones} stacks of Whetstones`
+        return `${gold.toLocaleString("en-US")} Gold<br/>${pentagons} <img src="images/icons/Pentagon_Gem.png" width="20" height="20"> ⇒ ${octagons} <img src="images/icons/Octagon_Gem.png" width="20" height="20"><br/>${ch1mats} stacks of Ch.1 Mats<br/>${whetstones} stacks of Whetstones`
     }
 
     _compute_stats() {
@@ -51,13 +62,13 @@ class NovasTerminal {
     // gold, drill, t1, tw
     add_facility(name) {
         if (name === "gold") {
-            this.num_goldgens += 1;
+            this.num_goldgens = Math.min(99,this.num_goldgens-1);
         } else if (name === "drill") {
-            this.num_drills += 1;
+            this.num_drills = Math.min(99,this.num_drills-1);
         } else if (name === "t1") {
-            this.num_t1 += 1;
+            this.num_t1 = Math.min(99,this.num_t1-1);
         } else if (name === "tw") {
-            this.num_twhet += 1;
+            this.num_twhet = Math.min(99,this.num_twhet-1);
         }
         this._compute_stats()
     }
@@ -102,6 +113,7 @@ const powercreated = document.getElementById("powercreated");
 const powerconsumed = document.getElementById("powerconsumed");
 const powerremaining = document.getElementById("powerremaining");
 
+const mainframe_level = document.getElementById("mainframe_level");
 const num_goldgens = document.getElementById("num_goldgens");
 const num_drills = document.getElementById("num_drills");
 const num_t1 = document.getElementById("num_t1");
@@ -113,6 +125,8 @@ const daily_output = document.getElementById("daily_output");
 const max_output = document.getElementById("max_output");
 
 // Event listeners
+const mainframeUp = document.getElementById("mainframeUp");
+const mainframeDown = document.getElementById("mainframeDown");
 const goldSub = document.getElementById("goldSub");
 const goldAdd = document.getElementById("goldAdd");
 const drillSub = document.getElementById("drillSub");
@@ -138,12 +152,13 @@ function updateDisplay() {
     powerconsumed.innerHTML = `${novasTerminal.powerconsumed}`;
     powerremaining.innerHTML = `${novasTerminal.powerremaining}`;
 
-    num_goldgens.innerHTML = `${novasTerminal.num_goldgens}x`;
-    num_drills.innerHTML = `${novasTerminal.num_drills}x`;
-    num_t1.innerHTML = `${novasTerminal.num_t1}x`;
-    num_twhet.innerHTML = `${novasTerminal.num_twhet}x`;
-    num_powercells.innerHTML = `${novasTerminal.num_powercells}x`;
-    num_grids.innerHTML = `${novasTerminal.num_grids}x`;
+    mainframe_level.innerHTML = `${novasTerminal.level}`
+    num_goldgens.value = `${novasTerminal.num_goldgens}`;
+    num_drills.value = `${novasTerminal.num_drills}`;
+    num_t1.value = `${novasTerminal.num_t1}`;
+    num_twhet.value = `${novasTerminal.num_twhet}`;
+    num_powercells.innerHTML = `${novasTerminal.num_powercells}`;
+    num_grids.innerHTML = `${novasTerminal.num_grids}`;
 
     daily_output.innerHTML = `${novasTerminal.get_daily_output()}`
     max_output.innerHTML = `${novasTerminal.get_max_capacity_output()}`
@@ -179,5 +194,93 @@ tmk1Add.addEventListener("click", () => {
 });
 twhetAdd.addEventListener("click", () => {
     novasTerminal.add_facility("tw");
+    updateDisplay();
+});
+
+mainframeUp.addEventListener("click", () => {
+    if (novasTerminal.level < 7) {
+        novasTerminal.level += 1;
+        novasTerminal.facilitycap = novasTerminal.level_to_cap[novasTerminal.level];
+        updateDisplay();
+    }
+});
+mainframeDown.addEventListener("click", () => {
+    if (novasTerminal.level > 1) {
+        novasTerminal.level -= 1;
+        novasTerminal.facilitycap = novasTerminal.level_to_cap[novasTerminal.level];
+        updateDisplay();
+    }
+});
+
+// Restricts input for the given textbox to the given inputFilter.
+function setInputFilter(textbox, inputFilter, errMsg) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout"].forEach(function(event) {
+        textbox.addEventListener(event, function(e) {
+            if (inputFilter(this.value)) {
+                // Accepted value
+                if (["keydown","mousedown","focusout"].indexOf(e.type) >= 0) {
+                    this.classList.remove("input-error");
+                    this.setCustomValidity("");
+                }
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                // Rejected value - restore the previous one
+                this.classList.add("input-error");
+                this.setCustomValidity(errMsg);
+                this.reportValidity();
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                // Rejected value - nothing to restore
+                this.value = "";
+            }
+        });
+    });
+}
+
+// get all input displays
+const collection = document.getElementsByClassName("inputdisplay");
+// Install input filters.
+for (var i = 0 ; i < collection.length; i++) {
+    setInputFilter(collection[i], function(value) {
+        return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 99); }, "Must be between 0 and 99");    
+}
+
+num_goldgens.addEventListener("input", function () {
+    console.log(this.value);
+    var tonumber = parseInt(this.value);
+    if (isNaN(tonumber)) {
+        return;
+    } 
+    novasTerminal.set_facility("gold", parseInt(this.value));
+    updateDisplay();
+});
+num_drills.addEventListener("input", function () {
+    console.log(this.value);
+    var tonumber = parseInt(this.value);
+    if (isNaN(tonumber)) {
+        return;
+    } 
+    novasTerminal.set_facility("drill", parseInt(this.value));
+    updateDisplay();
+});
+num_t1.addEventListener("input", function () {
+    console.log(this.value);
+    var tonumber = parseInt(this.value);
+    if (isNaN(tonumber)) {
+        return;
+    } 
+    novasTerminal.set_facility("t1", parseInt(this.value));
+    updateDisplay();
+});
+num_twhet.addEventListener("input", function () {
+    console.log(this.value);
+    var tonumber = parseInt(this.value);
+    if (isNaN(tonumber)) {
+        return;
+    } 
+    novasTerminal.set_facility("tw", parseInt(this.value));
     updateDisplay();
 });
